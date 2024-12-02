1f56c5a1e521faea7920285475937befef1d4a7e
Gokul21122001 <gokul.elumalai12@gmail.com>
2024-12-02 23:48:17 +0530

    Create Solution.java

---
+++ DivideTwoNumber/Solution.java
@@ -0,0 +1,41 @@
+public class practice {
+
+	public static void main(String[] args) {
+
+		Solution s1 = new Solution();
+		int divid = 2147483647;
+		int divic = 1;
+		// => 2147483647
+//		int divid = 10;
+//		int divic = 3;
+		System.out.println(s1.divide(divid, divic));
+	}
+
+}
+
+class Solution {
+	public int divide(int divident, int divison) {
+		if (divident == 0)
+			return 0;
+		int divide = (divident) / (divison);
+//		System.out.println(divident < Integer.MIN_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+		if (divident == Integer.MIN_VALUE && divison == -1)
+			return Integer.MAX_VALUE;
+		boolean sign = (divident < 0) ^ (divison < 0);
+
+		long absdivident = Math.abs((long) divide);
+		long absdivisor = Math.abs((long) absdivident);
+		long quot = 0;
+
+		while (absdivident >= absdivisor) {
+			long temp = absdivisor, multiple = 1;
+			while (absdivident >= (temp << 1)) {
+				temp <<=1;
+				
+			}
+		}
+
+		return divide;
+	}
+}
