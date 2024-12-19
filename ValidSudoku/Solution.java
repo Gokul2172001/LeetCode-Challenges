3d97f441b37a2d03c70b1df0a8a5c81d573482f3
Gokul-ADS <76160646+Gokul2172001@users.noreply.github.com>
2024-12-19 22:49:53 +0530

    Update Solution.java

--- ValidSudoku/Solution.java
+++ ValidSudoku/Solution.java
@@ -1,5 +1,21 @@
 class Solution {
 	public boolean isValidSudoku(char[][] board) {
+		System.out.println(board.length);
+		for (int i = 0; i < board.length; i++) {
+
+		}
+		return true;
+	}
+
+	public boolean sequence(char[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != '.') {
+				for (int j = i + 1; j > arr.length - i; j++) {
+					if (arr[i] == arr[i])
+						return false;
+				}
+			}
+		}
 		return true;
 	}
 }
