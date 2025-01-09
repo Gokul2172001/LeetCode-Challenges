d633cbee189d0fc0658cdaf15467261ac5da6992
Gokul-ADS <76160646+Gokul2172001@users.noreply.github.com>
2025-01-09 22:53:14 +0530

    Create Solution.java

---
+++ FindSmallestLetterGreaterThanTarget/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (letters[mid] > target) {
+                right = mid - 1; 
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left < letters.length ? letters[left] : letters[0];
+    }
+}
