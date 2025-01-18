95de435b2954e82be6f4aa1eb10e61a9581266a6
Gokul21122001 <gokul.elumalai12@gmail.com>
2025-01-18 23:29:14 +0530

    Create Solution.java

---
+++ RepeatedSubStringPattern/Solution.java
@@ -0,0 +1,9 @@
+
+class Solution {
+
+	public boolean repeatedSubstringPattern(String s) {
+         String doubledString = s + s;
+        
+        return doubledString.substring(1, doubledString.length() - 1).contains(s);
+    }
+}
