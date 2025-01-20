a5ecc8cee889d171340d77c3ab48dc54d540d68f
Gokul21122001 <gokul.elumalai12@gmail.com>
2025-01-20 23:05:17 +0530

    Create Solution.java

---
+++ MaxConsecutivesOnes/Solution.java
@@ -0,0 +1,23 @@
+
+class Solution {
+
+	public int findMaxConsecutiveOnes(int[] nums) {
+    
+		int max = 0; 
+        int current = 0;
+
+        for (int num : nums) {
+        
+        	if (num == 1) {
+                current++; 
+            } else {
+                max = Math.max(max, current); 
+                current = 0; 
+            }
+        }
+        
+        max = Math.max(max, current);
+        
+        return max;
+    }
+}
