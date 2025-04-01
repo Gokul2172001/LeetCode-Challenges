924bcc77fe6bcb1b346f7664f8c8bf59f34473f9
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2025-04-01 23:47:13 +0530

    Update Solution.java

--- CanPlaceFlowers/Solution.java
+++ CanPlaceFlowers/Solution.java
@@ -3,15 +3,14 @@ class Solution {
 
 	public boolean canPlaceFlowers(int[] flowerbed, int n) {
     
-		if (n == 0) 
+	if (n == 0) 
         return true;
         
         int count = 0;
         
         for (int i = 0; i < flowerbed.length; i++) {
            
-            if (flowerbed[i] == 0 && 
-                (i == 0 || flowerbed[i - 1] == 0) && 
+            if (flowerbed[i] == 0 &&                 (i == 0 || flowerbed[i - 1] == 0) && 
                 (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
                 
                 flowerbed[i] = 1; 
@@ -20,6 +19,7 @@ class Solution {
                 if (count >= n) 
                 return true; 
                 i++; 
+		    
                  }
         }
         
