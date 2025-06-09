b9aea3a6725104e522b5578b692de9a0847a8b7d
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2025-06-09 23:14:58 +0530

    Update Solution.java

--- ClimbingStairs/Solution.java
+++ ClimbingStairs/Solution.java
@@ -2,16 +2,18 @@ class Solution {
     
     public int climbStairs(int n) {
     
-        if(n==1)
+        if(n==1) {
         return 1;
-
+    }
         int firstVal =1;
         int secondVal =1; 
 
         for(int i=2; i<=n; i++) {
         
             int temp=firstVal+secondVal;
+            
             firstVal =secondVal;
+            
             secondVal =temp;   
         }
         
